298769683eb86525c3446ae3b51ef7c6ae87baa6
Quan <n.dinh@tdsoftware.de>
2019-08-28 16:13:42 +0200

    Initial Commit

---
+++ VS2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VS2019
+//
+//  Created by Quân Đinh on 28.08.19.
+//  Copyright © 2019 Quân Đinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ VS2019Tests/VS2019Tests.swift
@@ -0,0 +1,34 @@
+//
+//  VS2019Tests.swift
+//  VS2019Tests
+//
+//  Created by Quân Đinh on 28.08.19.
+//  Copyright © 2019 Quân Đinh. All rights reserved.
+//
+
+import XCTest
+@testable import VS2019
+
+class VS2019Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
